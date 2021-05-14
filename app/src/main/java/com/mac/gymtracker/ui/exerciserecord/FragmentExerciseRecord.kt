9956b97d51f3513def78b06426b37dffbd998e52
MacNew <newmac1000@gmail.com>
2021-05-14 16:07:49 +0545

    numberpicker added

--- app/src/main/java/com/mac/gymtracker/ui/exerciserecord/FragmentExerciseRecord.kt
+++ app/src/main/java/com/mac/gymtracker/ui/exerciserecord/FragmentExerciseRecord.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.mac.gymtracker.databinding.FragmentExerciseRecordBinding
-import com.mac.gymtracker.utils.showToast
+import java.lang.Exception
 
 class FragmentExerciseRecord : Fragment() {
     private lateinit var viewmodle: ExerciseRecordViewModle
@@ -26,7 +26,24 @@ class FragmentExerciseRecord : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         var id: String? = FragmentExerciseRecordArgs.fromBundle(arguments!!).exerciseId
-        activity!!.applicationContext.showToast(id!!)
+        setWeightAndRep()
+    }
+
+    private fun setWeightAndRep() {
+        val weightData= arrayOfNulls<String>(100)
+        for(i in 1..weightData.size) {
+            weightData[i-1] = i.toString()
+        }
+        val repData = arrayOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+        binding!!.numberPickerWeight.minValue = 1
+        binding!!.numberPickerWeight.maxValue = weightData.size
+        binding!!.numberPickerWeight.displayedValues= weightData
+        binding!!.numberPickerWeight.value = 1
+
+        binding!!.numberPickerRep.minValue = 1
+        binding!!.numberPickerRep.maxValue = repData.size
+        binding!!.numberPickerRep.displayedValues= repData
+        binding!!.numberPickerRep.value = 1
     }
 
 }
